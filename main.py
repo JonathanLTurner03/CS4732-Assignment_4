de7c81f9e609cf55049609ad8c1a3901cbaf48a0
Jonathan Turner <turner@atlantissrv.com>
2024-10-20 12:49:52 -0400

    Added some more criteria and optimizer (using adam)

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
+import torch.nn as nn
+from torch.optim import Adam
 
 # Possibly add transforms to improve data?
+
+# Add a way to check for torch GPU and use it if available.
 
 # Loads the dataset
 train_dataset = datasets.ImageFolder(root='DS_IDRID/Train', transform=transforms.ToTensor())
@@ -15,3 +19,13 @@ test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=F
 
 ## Loads the resnet50 model.
 model = models.resnet50(pretrained=True)
+num_features = model.fc.in_features
+
+# Converts to a binary classifier.
+model.fc = nn.Linear(num_features, 2)
+#model = model.to(device) # Gotta get torchGPU working for this first.
+
+# Setup optimizers.
+crit = nn.CrossEntropyLoss()
+optim = Adam(model.parameters(), lr=0.001) # TODO Setup loop to test different rates for assgn
+
